32ad2b788761a6e4e61578072f2eb50026a1137e
Michael Taylor <michael.taylor.gamedev@gmail.com>
2024-07-22 23:20:58 -0500

    August event

--- src/components/next-event/NextEvent.tsx
+++ src/components/next-event/NextEvent.tsx
@@ -21,7 +21,7 @@ const NextEvent: React.FC = () => {
     }
 
   //year, month, day
-  nextDate.setFullYear(2024, Months.June, 27);
+  nextDate.setFullYear(2024, Months.August, 22);
 
   return (
     <div className="flex justify-center items-center font-custom my-10">
